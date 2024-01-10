b8c9c792a1e04b09678b16614257fe9776bcdbf0
Ahmed Mansour <git@mansour.fi>
2024-01-10 15:02:01 +0200

    init authentication using phoenix auth generator
    argon2 hasing mechanism used for increased security

--- lib/mvp_match_code_challenge_web/router.ex
+++ lib/mvp_match_code_challenge_web/router.ex
@@ -1,6 +1,8 @@
 defmodule MvpMatchCodeChallengeWeb.Router do
   use MvpMatchCodeChallengeWeb, :router
 
+  import MvpMatchCodeChallengeWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule MvpMatchCodeChallengeWeb.Router do
     plug :put_root_layout, html: {MvpMatchCodeChallengeWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -39,6 +42,44 @@ defmodule MvpMatchCodeChallengeWeb.Router do
 
       live_dashboard "/dashboard", metrics: MvpMatchCodeChallengeWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", MvpMatchCodeChallengeWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{MvpMatchCodeChallengeWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", MvpMatchCodeChallengeWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{MvpMatchCodeChallengeWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", MvpMatchCodeChallengeWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{MvpMatchCodeChallengeWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
