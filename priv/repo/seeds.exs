a33929f5e6df978ceda9163fb4f97303d4be2d97
Ahmed Mansour <git@mansour.fi>
2024-01-13 15:44:33 +0200

    add db seed script for testing

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,52 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     MvpMatchCodeChallenge.Repo.insert!(%MvpMatchCodeChallenge.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias MvpMatchCodeChallenge.Repo
+alias MvpMatchCodeChallenge.Accounts.User
+alias MvpMatchCodeChallenge.Products.Product
+import Ecto.Query
+
+# Insert Users
+users = [
+  %{username: "user_buyer", password: "Hello world!", role: :buyer, deposit: 1000},
+  %{username: "user_seller", password: "Hello world!", role: :seller}
+]
+
+Enum.each(users, fn user_attrs ->
+  %User{}
+  |> User.registration_changeset(user_attrs)
+  |> Repo.insert()
+end)
+
+sellers = Repo.all(from u in User, where: u.role == ^:seller)
+
+products = [
+  %{
+    product_name: "Product 1",
+    amount_available: 10,
+    cost: "10",
+    seller_id: Enum.at(sellers, 1).id
+  },
+  %{
+    product_name: "Product 2",
+    amount_available: 15,
+    cost: "15",
+    seller_id: Enum.at(sellers, 1).id
+  },
+  %{
+    product_name: "Product 3",
+    amount_available: 20,
+    cost: "20",
+    seller_id: Enum.at(sellers, 1).id
+  }
+]
+
+Enum.each(products, fn product_attrs ->
+  %Product{}
+  |> Product.changeset(product_attrs)
+  |> Repo.insert()
+end)
+
+total_products = Repo.all(Product) |> Enum.count()
+total_users = Repo.all(User) |> Enum.count()
+
+IO.inspect("Seeds inserted successfully!")
+IO.inspect("Users: #{total_users}")
+IO.inspect("Products: #{total_products}")
